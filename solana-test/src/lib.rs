c7679b7fcd9596992b2321f261917f1d0930f29b
divyeshkakadiya-invenyv <divyesh.kakadiya@inventyv.com>
2026-05-06 18:04:56 +0530

    Enhance Transfer functionality by adding TransferInstruction and TransferData structs; implement process_transfer and create_account_cpi functions

--- solana-test/src/lib.rs
+++ solana-test/src/lib.rs
@@ -1,6 +1,8 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::account_info::next_account_info;
-use solana_program::entrypoint;
+use solana_program::example_mocks::solana_sdk::system_instruction::{self, create_account};
+use solana_program::program::invoke;
+use solana_program::{entrypoint, lamports};
 use solana_program::{
     entrypoint::{__AccountInfo, ProgramResult},
     program_error::ProgramError,
@@ -15,6 +17,15 @@ pub enum CustomInstruction {
     Decrement,
     Reset,
     TransferAuthority,
+}
+
+pub enum TransferInstruction {
+    TransferSol { amount: u64 },
+}
+
+#[derive(Debug, BorshDeserialize, BorshSerialize)]
+pub struct TransferData {
+    amount: u64,
 }
 
 impl CustomInstruction {
@@ -119,10 +130,10 @@ pub fn process_intruction(
             }
 
             let new_authority_account = next_account_info(accounts_iter)?;
-            
+
             if !new_authority_account.is_signer {
                 return Err(ProgramError::MissingRequiredSignature);
-            }   
+            }
 
             counter.authority = *new_authority_account.key;
             counter.serialize(&mut *data)?;
@@ -130,4 +141,79 @@ pub fn process_intruction(
     }
 
     Ok(())
+}
+
+pub fn create_account_cpi(
+    program_id: &Pubkey,
+    accounts: &[__AccountInfo],
+    space: u64,
+) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+    let payer = next_account_info(account_info_iter)?;
+    let new_account = next_account_info(account_info_iter)?;
+    let system_program = next_account_info(account_info_iter)?;
+
+    let owner = program_id;
+
+    let rent = Rent::get()?;
+    let lamports = rent.minimum_balance(space as usize);
+
+    let ix = create_account(payer.key, new_account.key, lamports, space, owner);
+
+    invoke(
+        &ix,
+        &[payer.clone(), new_account.clone(), system_program.clone()],
+    )
+}
+
+pub fn process_transfer(
+    _program_id: &Pubkey,
+    accounts: &[__AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let account_into_iter = &mut accounts.iter();
+
+    let sender = next_account_info(account_into_iter)?;
+    let recipient = next_account_info(account_into_iter)?;
+    let system_program = next_account_info(account_into_iter)?;
+
+    // let amount = u64::from_le_bytes(
+    //     instruction_data
+    //         .get(..8)
+    //         .ok_or(ProgramError::InvalidInstructionData)?
+    //         .try_into()
+    //         .map_err(|_| ProgramError::InvalidInstructionData)?,
+    // ); // instruction data = [10 , 0 , 0 , 0 , 0 , 0 , 0 , 0] => amount = 10 //manual without borsh
+
+    let data = TransferData::try_from_slice(instruction_data)?;
+    let amount = data.amount;
+
+    if sender.is_signer == false {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    let ix = system_instruction::transfer(sender.key, recipient.key, amount);
+
+    invoke(
+        &ix,
+        &[sender.clone(), recipient.clone(), system_program.clone()],
+    )?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use solana_program::rent::Rent;
+
+    use super::*;
+
+    #[test]
+    pub fn test_rent() {
+        let rent = Rent::default();
+        let lamports = rent.minimum_balance(100);
+
+        println!("Minimum balance for 100 bytes: {}", lamports);
+    }
 }
